3e417fd53ec899e447e54f77a532e6964bb56b3e
Larimar4you <Zajanezlaja@gmail.com>
2025-01-09 23:12:40 +0100

    git push

--- js/task-1.js
+++ js/task-1.js
@@ -1,43 +1,10 @@
 /* #завдання */
 /*
-Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. 
-Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів. 
-У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
-
-
+Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка 
+елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 */
 `use strict`;
-
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount());
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance());
-console.log(customer.getOrders());
